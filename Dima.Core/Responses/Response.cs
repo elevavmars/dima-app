ffb8469ed32bdf7eb82f7cb7cfb3c10e8127c30a
mars <marcus.metelis@outlook.com>
2024-10-16 06:12:59 +0100

    first commit

---
+++ Dima.Core/Responses/Response.cs
@@ -0,0 +1,24 @@
+using System.Text.Json.Serialization;
+
+namespace Dima.Core.Responses;
+
+public class Response<TData>
+{
+    private readonly int _code;
+
+    [JsonConstructor]
+    public Response() => _code = Configuration.DefaultStatusCode;
+
+    public Response(TData? data, int code = 200, string? message = null)
+    {
+        Data = data;
+        Message = message;
+        _code = Configuration.DefaultStatusCode;
+    }
+
+    public TData? Data { get; set; }
+    public string? Message { get; set; }
+
+    [JsonIgnore]
+    public bool IsSuccess => _code is >= 200 and <= 299;
+}
