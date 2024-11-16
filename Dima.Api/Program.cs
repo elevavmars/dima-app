36ae4d024e230d042eafb0fae8423f2bbcea140a
mars <marcus.metelis@outlook.com>
2024-11-16 16:59:23 -0400

    add app.MapEndpoints()

--- Dima.Api/Program.cs
+++ Dima.Api/Program.cs
@@ -1,4 +1,5 @@
 using Dima.Api.Data;
+using Dima.Api.Endpoints;
 using Dima.Api.Handlers;
 using Dima.Core.Handlers;
 using Dima.Core.Models;
@@ -29,70 +30,7 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.MapPost(
-    "/v1/categories", async (CreateCategoryRequest request, ICategoryHandler handler) 
-    => await handler.CreateAsync(request))
-    .WithName("Categories: Create")
-    .WithSummary("Cria uma nova categoria")
-    .Produces<Response<Category>>();
-
-app.MapPut(
-    "/v1/categories/{id}", async (long id,
-            UpdateCategoryRequest request,
-            ICategoryHandler handler)
-        =>
-    {
-        request.Id = id;
-        return await handler.UpdateAsync(request);
-    })
-    .WithName("Categories: Update")
-    .WithSummary("Atualiza uma categoria")
-    .Produces<Response<Category?>>();
-
-app.MapDelete(
-    "/v1/categories/{id}", async (long id,
-            ICategoryHandler handler)
-        =>
-    {
-        var request = new DeleteCategoryRequest
-        {
-            Id = id,
-            UserId = "teste@own"
-        };
-        return await handler.DeleteAsync(request);
-    })
-    .WithName("Categories: Delete")
-    .WithSummary("Deleta uma categoria")
-    .Produces<Response<Category?>>();
-
-app.MapGet(
-        "/v1/categories/", async (ICategoryHandler handler)
-            =>
-        {
-            var request = new GetAllCategoriesRequest()
-            {
-                UserId = "teste@own"
-            };
-            return await handler.GetAllAsync(request);
-        })
-    .WithName("Categories: Get All")
-    .WithSummary("Retorna todas as categorias de um usuário")
-    .Produces<PagedResponse<List<Category>?>>();
-
-app.MapGet(
-        "/v1/categories/{id}", async (long id,
-                ICategoryHandler handler)
-            =>
-        {
-            var request = new GetCategoryByIdRequest
-            {
-                Id = id,
-                UserId = "teste@own"
-            };
-            return await handler.GetByIdAsync(request);
-        })
-    .WithName("Categories: Get by Id")
-    .WithSummary("Retorna uma categoria")
-    .Produces<Response<Category?>>();
+app.MapGet("/", () => new { message = "OK" });
+app.MapEndpoints();
 
 app.Run();
